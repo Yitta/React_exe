1b09ea2b5f557ad3ccc5166e060c5631cc7b7783
Yitta <abecc1995@gmail.com>
2017-08-06 23:11:38 +1000

    finish tabbar bottom fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Tab,
+    TabBody,
+    TabBar,
+    TabBarItem,
+    TabBarIcon,
+    TabBarLabel,
+    Article,Button} from 'react-weui'
+import 'weui';
+import './react-weui.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
 
-export default App;
+import IconButton from './icon_nav_cell.png';
+import IconMsg from './icon_nav_msg.png';
+import IconArticle from './icon_nav_article.png';
+
+export default class TabBarAutoDemo extends React.Component {
+    render() {
+        return (
+          <div className="user1">
+            <Tab type="tabbar">
+                <TabBarItem icon={<img src={IconButton}/>} label="Tab1">
+                    <Article>
+                        <h1>Page 1</h1>
+                        <section>
+                            <h2 className="title">Heading</h2>
+                            <section>
+                                <h3>1.1 Title</h3>
+                                <Button>hello</Button>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute</p>
+                            </section>
+                        </section>
+                    </Article>
+                </TabBarItem>
+                <TabBarItem icon={<img src={IconMsg}/>} label="Tab2">
+                    <Article>
+                        <h1>Page 2</h1>
+                        <section>
+                            <h2 className="title">Heading</h2>
+                            <section>
+                                <h3>2.1 Title</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute</p>
+                            </section>
+                        </section>
+                    </Article>
+                </TabBarItem>
+                <TabBarItem icon={<img src={IconArticle}/>} label="Tab3">
+                    <Article>
+                        <h1>Page 3</h1>
+                        <section>
+                            <h2 className="title">Heading</h2>
+                            <section>
+                                <h3>3.1 Title</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                    consequat. Duis aute</p>
+                            </section>
+                        </section>
+                    </Article>
+                </TabBarItem>
+            </Tab>
+          </div>
+        );
+    }
+};
